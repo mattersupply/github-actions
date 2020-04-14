4b5063cac01298d4a4451e819bdb6e105989c49d
Marc Ammann <marc@mattersupply.co>
2020-04-13 21:48:21 -0700

    added node mdoules

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const github = require("@actions/github");
 const aws = require("aws-actions-configure-aws-credentials");
 
 try {
+  console.log("Hello");
   // // `who-to-greet` input defined in action metadata file
   // const nameToGreet = core.getInput("who-to-greet");
   // console.log(`Hello ${nameToGreet}!`);
   // const time = new Date().toTimeString();
   // core.setOutput("time", time);
   // // Get the JSON webhook payload for the event that triggered the workflow
+
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
   return aws();
